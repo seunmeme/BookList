65ffaa5477340e69e4fc449c8c17b2048a9793a7
Seun Odewale <seunodewale.zone5@gmail.com>
2021-02-11 00:25:00 +0100

    add Book model

---
+++ BookList/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookList.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public Book()
+        {
+        }
+    }
+}
